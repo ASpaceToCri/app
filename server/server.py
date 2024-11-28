603087885271691f2627bfafb1b3e7c64bdf0bd3
Cyang8980 <78817877+Cyang8980@users.noreply.github.com>
2024-11-28 15:55:22 -0500

    added some dependencies

--- server/server.py
+++ server/server.py
@@ -1,10 +1,10 @@
 # importing Flask and jsonify from the flask package
 from flask import Flask, jsonify
-
+from flask_cors import CORS
 # app instance
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/api/home", methods = ['GET'])
 
 def return_home():
@@ -13,4 +13,4 @@ def return_home():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port = 8080)
